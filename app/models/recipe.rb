115d87055d4908665d57dd80737652c3feee7071
Alphy Gacheru <alphygacheru@gmail.com>
2022-11-29 17:10:09 +0300

    Validate recipe attributes

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,10 @@
 class Recipe < ApplicationRecord
   belongs_to :user
+
+  # validations
+  validates :name, presence: true, length: { maximum: 250 }
+  validates :preparation_time, presence: true, length: { maximum: 250 }
+  validates :cooking_time, presence: true, length: { maximum: 250 }
+  validates :description, presence: true, length: { maximum: 500 }
+  validates :public, inclusion: [true, false]
 end
