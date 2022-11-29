809e308c7e9a2ae948c8d28ae9049c1f292f8b51
Alphy Gacheru <alphygacheru@gmail.com>
2022-11-29 19:07:44 +0300

    Add the functionality to create and save a recipe to the db

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -4,4 +4,22 @@ class RecipesController < ApplicationController
   def new
     @recipe = Recipe.new
   end
+
+  def create
+    @recipe = Recipe.new(recipe_params)
+    @recipe.user = current_user
+
+    if @recipe.save
+      flash[:notice] = 'Recipe successfully created'
+      redirect_to recipes_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:name, :preparation_time, :cooking_time, :description, :public)
+  end
 end
