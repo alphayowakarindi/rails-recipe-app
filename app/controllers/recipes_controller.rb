333b3efeabaedf3104709b4ae0c3149e4eab7f35
Alphy Gacheru <alphygacheru@gmail.com>
2022-11-30 13:35:23 +0300

    Display recipe's attributes value dynamically on recipe show page

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -9,6 +9,10 @@ class RecipesController < ApplicationController
     @recipe = Recipe.new
   end
 
+  def show
+    @recipe = Recipe.find(params[:id])
+  end
+
   def create
     @recipe = Recipe.new(recipe_params)
     @recipe.user = current_user
