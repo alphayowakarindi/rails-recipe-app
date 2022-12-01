d7a625027af9bbcaec202c05a18cb0dfd5c56912
Alphy Gacheru <alphygacheru@gmail.com>
2022-12-01 18:19:02 +0300

    Add quantity column in foods table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_01_145044) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_01_151812) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,6 +20,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_01_145044) do
     t.integer "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "quantity"
   end
 
   create_table "recipe_foods", force: :cascade do |t|
