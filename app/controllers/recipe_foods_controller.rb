18647f1dad8250425b5219ea6ca1b1f5475fa4cb
Alphy Gacheru <alphygacheru@gmail.com>
2022-12-02 10:00:08 +0300

    Display a recipe's ingredients on the recipe show page

--- app/controllers/recipe_foods_controller.rb
+++ app/controllers/recipe_foods_controller.rb
@@ -7,6 +7,7 @@ class RecipeFoodsController < ApplicationController
   def create
     @recipe = Recipe.find(params[:recipe_id])
     @recipe_food = @recipe.recipe_foods.create(recipe_food_params)
+
     if @recipe_food.save
       flash[:notice] = 'Ingredient added successfully.'
       redirect_to @recipe
